8e19e4dfe83f5fe7ab745b998ea2dcc700763fba
Drew.Slone <d.slone317@gmail.com>
2020-12-03 13:19:28 -0800

    Create Assignment_Inheritance.py

---
+++ Assignment_Inheritance.py
@@ -0,0 +1,22 @@
+
+class Body:
+    Current_Car_Type = "1999 Skyline"
+    HitBox_Type = "Hybrid"
+
+class Color(Body):
+    Selected_Decal = "Rizer Black Market Decal"
+    Current_Primary_Color = "Sky Blue"
+    Current_Secondary_Color = "Black"
+
+class Wheels(Body):
+    Current_Wheels = "Cristiano"
+    Current_Color = "Black"
+
+class Boost(Body):
+    Current_Boost = "Flamethrower"
+    Current_Color = "Black"
+
+    
+    
+    
+
